55cba715080a475710979b8368f87cc394833853
Pushpendraasharma <pushpendra.sharma@dotsquares.com>
2022-11-09 20:45:50 +0530

    uncomment pytest

--- devenv.sh
+++ devenv.sh
@@ -1,5 +1,5 @@
  #! /bin/sh
-export env_aws_access_key= xxx 
-export env_aws_secret_access_key= xxx
+export env_aws_access_key= AKIAYYHDLHOCMCWZOW3J 
+export env_aws_secret_access_key= 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
 
 
