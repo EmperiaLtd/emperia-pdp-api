8304a5165b3dd2afeefe17873101ae3f2b75849a
Pushpendraasharma <pushpendra.sharma@dotsquares.com>
2022-11-09 22:02:47 +0530

    change api gateway

--- terraform/app-deploy/api-gateway.tf
+++ terraform/app-deploy/api-gateway.tf
@@ -13,18 +13,84 @@ resource "aws_apigatewayv2_api" "Emperia-PDP-gateway" {
 }
 
 
-resource "aws_apigatewayv2_stage" "Emperia-PDP-gateway" {
-  api_id      = aws_apigatewayv2_api.Emperia-PDP-gateway.id
-  name        = local.stage
-  auto_deploy = true
+resource "aws_apigatewayv2_authorizer" "Emperia-PDP-gateway" {
+  api_id           = aws_apigatewayv2_api.Emperia-PDP-gateway.id
+  authorizer_type  = "JWT"
+  identity_sources = ["$request.header.Authorization"]
+  name             = "Emperia-PDP-authorizer-${local.stage}"
 
-  default_route_settings {
-    throttling_burst_limit = 5000
-    throttling_rate_limit  = 10000
+  jwt_configuration {
+    audience = [local.authorization_audience]
+    issuer   = local.authorization_issuer
   }
 }
+
+resource "aws_apigatewayv2_integration" "Emperia-PDP-integration" {
+  api_id           = aws_apigatewayv2_api.Emperia-PDP-gateway.id
+  integration_type = "AWS_PROXY"
+
+  connection_type        = "INTERNET"
+  integration_method     = "POST"
+  integration_uri        = aws_lambda_function.Emperia-PDP-lambda-function.invoke_arn
+  payload_format_version = "2.0"
+}
+
 resource "aws_apigatewayv2_route" "Emperia-PDP-api-get-route" {
-  api_id             = aws_apigatewayv2_api.Emperia-PDP-gateway
-  route_key          = "GET /"
-  target             = "integrations/${aws_apigatewayv2_integration.Emperia-PDP-integration}"
+  api_id             = aws_apigatewayv2_api.Emperia-PDP-gateway.id
+  route_key          = "GET /api/{proxy+}"
+  authorization_type = "JWT"
+  authorizer_id      = aws_apigatewayv2_authorizer.Emperia-PDP-gateway.id
+  target             = "integrations/${aws_apigatewayv2_integration.Emperia-PDP-integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "Emperia-PDP-api-put-route" {
+  api_id             = aws_apigatewayv2_api.Emperia-PDP-gateway.id
+  route_key          = "PUT /api/{proxy+}"
+  authorization_type = "JWT"
+  authorizer_id      = aws_apigatewayv2_authorizer.Emperia-PDP-gateway.id
+  target             = "integrations/${aws_apigatewayv2_integration.Emperia-PDP-integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "Emperia-PDP-api-delete-route" {
+  api_id             = aws_apigatewayv2_api.Emperia-PDP-gateway.id
+  route_key          = "DELETE /api/{proxy+}"
+  authorization_type = "JWT"
+  authorizer_id      = aws_apigatewayv2_authorizer.Emperia-PDP-gateway.id
+  target             = "integrations/${aws_apigatewayv2_integration.Emperia-PDP-integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "Emperia-PDP-api-post-route" {
+  api_id             = aws_apigatewayv2_api.Emperia-PDP-gateway.id
+  route_key          = "POST /api/{proxy+}"
+  authorization_type = "JWT"
+  authorizer_id      = aws_apigatewayv2_authorizer.Emperia-PDP-gateway.id
+  target             = "integrations/${aws_apigatewayv2_integration.Emperia-PDP-integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "Emperia-PDP-docs-route" {
+  api_id             = aws_apigatewayv2_api.Emperia-PDP-gateway.id
+  route_key          = "GET /docs"
+  authorization_type = "NONE"
+  target             = "integrations/${aws_apigatewayv2_integration.Emperia-PDP-integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "Emperia-PDP-redoc-route" {
+  api_id             = aws_apigatewayv2_api.Emperia-PDP-gateway.id
+  route_key          = "GET /redoc"
+  authorization_type = "NONE"
+  target             = "integrations/${aws_apigatewayv2_integration.Emperia-PDP-integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "Emperia-PDP-openapi-route" {
+  api_id             = aws_apigatewayv2_api.Emperia-PDP-gateway.id
+  route_key          = "GET /openapi.json"
+  authorization_type = "NONE"
+  target             = "integrations/${aws_apigatewayv2_integration.Emperia-PDP-integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "Emperia-PDP-credit-route" {
+  api_id             = aws_apigatewayv2_api.Emperia-PDP-gateway.id
+  route_key          = "POST /webhook/credit/{proxy+}"
+  authorization_type = "NONE"
+  target             = "integrations/${aws_apigatewayv2_integration.Emperia-PDP-integration.id}"
 }
