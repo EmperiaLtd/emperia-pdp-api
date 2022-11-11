85a28e23abb66cd5fa4f365f5588081ac9ee4218
Pushpendraasharma <pushpendra.sharma@dotsquares.com>
2022-11-11 17:59:02 +0530

    modify lambda.tf

--- terraform/app-deploy/variables.tf
+++ terraform/app-deploy/variables.tf
@@ -26,3 +26,27 @@ variable "backend_remote_state_s3_bucket" {
   default = "terraform-state-emperia-pdp"
 }
 
+variable "dev_authorization_issuer"{
+  default = "https://emperia.eu.auth0.com/"
+
+}
+variable "staging_authorization_issuer"{
+    default = "https://emperia-staging.eu.auth0.com/"
+}
+variable "prod_authorization_issuer"{
+  default = "https://emperia-production.eu.auth0.com/"
+}
+ variable "dev_authorization_audience" {
+  default = "https://artemis-apollo-jwt-authorizer"
+}
+ variable "staging_authorization_audience" {
+  default = "https://artemis-apollo-jwt-authorizer-staging"
+}
+ variable "prod_authorization_audience" {
+  default = "https://artemis-apollo-jwt-authorizer-production"
+}
+locals {
+  stage       = terraform.workspace == "default" ? "dev" : terraform.workspace
+  authorization_issuer =  terraform.workspace == "production" ? var.prod_authorization_issuer : (terraform.workspace == "staging" ? var.staging_authorization_issuer : var.dev_authorization_issuer)
+  authorization_audience = terraform.workspace == "production" ? var.prod_authorization_audience : (terraform.workspace == "staging" ? var.staging_authorization_audience: var.dev_authorization_audience)
+}
