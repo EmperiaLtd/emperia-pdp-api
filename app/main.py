050b5117295573b112d308dedc33a82df2a74682
Pushpendraasharma <pushpendra.sharma@dotsquares.com>
2022-11-11 19:20:29 +0530

    modify lambda.tf

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from mangum import Mangum
-
+from app.config import settings
 
 app = FastAPI(title="emperia-pdp-API",
               root_path="/")
@@ -12,4 +12,4 @@ async def read_root():
         "status_code": 200,
         "message": "Hello World"}
 
-handler = Mangum(app, api_gateway_base_path="/")
+handler = Mangum(app, api_gateway_base_path="/".format(settings.stage_name))
