d492b5953050bff41653afbd2d5c3ff7329a9ca9
Pushpendraasharma <pushpendra.sharma@dotsquares.com>
2022-11-11 13:09:01 +0530

    modify lambda.tf

--- terraform/app-deploy/lambda.tf
+++ terraform/app-deploy/lambda.tf
@@ -57,16 +57,19 @@ resource "aws_iam_role" "lambda" {
   name               = "${local.prefix}-lambda-role-${local.stage}"
   assume_role_policy = <<EOF
 {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Action": "sts:AssumeRole",
-                "Principal": {
-                    "Service": "lambda.amazonaws.com"
-                },
-                "Effect": "Allow"
-            }
-        ]
+   "Version": "2012-10-17",
+   "Statement": [
+      {
+         "Effect": "Allow",
+         "Principal": {
+            "Service": [
+               "lambda.amazonaws.com",
+               "edgelambda.amazonaws.com"
+            ]
+         },
+         "Action": "sts:AssumeRole"
+      }
+   ]
 }
     EOF
 }
