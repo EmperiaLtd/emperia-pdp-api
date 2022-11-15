5d78985c9c762418cb6bc9bb4b66168da5da1e44
Pushpendraasharma <pushpendra.sharma@dotsquares.com>
2022-11-15 19:23:03 +0530

    add new files

--- app/db/product.py
+++ app/db/product.py
@@ -1,19 +1,23 @@
 import app.db.database as db
-import json 
+import json
 import os
 database = db.redis
 # product_collection = database.get_collection("products_collection")
 
-def load_from_db(pid, market, org_id):
-  pid2 = f"{org_id}_{market}_{pid}" 
-  db_obj = database.get(pid2) #get the pid from database 
-  if db_obj is None: return None
-  json_data = json.loads(db_obj.encode('utf-8'))
-  return json_data
 
-def load_from_db_2(market,org_id):
-  market2=f"{org_id}_{market}" #get the market from database 
-  db_obj_2 = database.get(market2)
-  if db_obj_2 is None: return None
-  json_data = json.loads(db_obj_2.encode('utf-8'))
-  return json_data  
+def load_from_db(pid, market, org_id):
+    pid2 = f"{org_id}_{market}_{pid}"
+    db_obj = database.get(pid2)  # get the pid from database
+    if db_obj is None:
+        return None
+    json_data = json.loads(db_obj.encode('utf-8'))
+    return json_data
+
+
+def load_from_db_2(market, org_id):
+    market2 = f"{org_id}_{market}"  # get the market from database
+    db_obj_2 = database.get(market2)
+    if db_obj_2 is None:
+        return None
+    json_data = json.loads(db_obj_2.encode('utf-8'))
+    return json_data
--- app/router/product.py
+++ app/router/product.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Body, status, HTTPException
 from app.db.product import (
     load_from_db,
-   load_from_db_2,
+    load_from_db_2,
 )
 
 router = APIRouter()
@@ -12,20 +12,22 @@ router = APIRouter()
 #         "message": message
 #     }
 
+
 @router.get("/{org_id}/{market}", response_description="Products retrieved")
-async def get_products(market,org_id):
-    products = load_from_db_2(market,org_id)
+async def get_products(market, org_id):
+    products = load_from_db_2(market, org_id)
     if products:
-        return {"data" : products, "status":200,"message":
-                             "Products data retrieved successfully"}
-    return {"data":products, "status":200, "message" :"Empty list returned"}
+        return {"data": products, "status": 200, "message":
+                "Products data retrieved successfully"}
+    return {"data": products, "status": 200, "message": "Empty list returned"}
 
 
-@router.get("/{org_id}/{market}/{pid}", response_description="Product data retrieved")
+@router.get("/{org_id}/{market}/{pid}",
+            response_description="Product data retrieved")
 async def get_product_data(pid, market, org_id):
     product = load_from_db(pid, market, org_id)
     if product:
-        return {"data" : product, "status":200,"message":
-                             "Products data retrieved successfully"}
+        return {"data": product, "status": 200, "message":
+                "Products data retrieved successfully"}
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                         detail="Product doesn't exist.")
