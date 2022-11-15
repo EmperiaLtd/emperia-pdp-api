49329af59044f01a46d32bd4c9f11fc2551a57e8
Pushpendraasharma <pushpendra.sharma@dotsquares.com>
2022-11-15 20:35:55 +0530

    add new files

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-# from mangum import Mangum
+from mangum import Mangum
 from app.config import settings
 from app.router import (product)
-app = FastAPI(title="emperia-pdp-API")#,root_path="/{}".format(settings.stage_name))
+app = FastAPI(title="emperia-pdp-API",
+              root_path="/{}".format(settings.stage_name))
 
 app.include_router(product.router, tags=["product"], prefix="/api/product")
 
@@ -13,4 +14,4 @@ async def read_root():
         "status_code": 200,
         "message": "Emperia PDP REST API"}
 
-# handler = Mangum(app, api_gateway_base_path="/{}".format(settings.stage_name))
+handler = Mangum(app, api_gateway_base_path="/{}".format(settings.stage_name))
