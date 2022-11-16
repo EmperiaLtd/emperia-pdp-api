14af412ed3b79d5318d4908e369184fadefe9cb3
Pushpendraasharma <pushpendra.sharma@dotsquares.com>
2022-11-16 15:10:37 +0530

    add new files

--- app/config.py
+++ app/config.py
@@ -7,6 +7,6 @@ class Settings(BaseSettings):
     aws_region: str = "eu-west-2"
     stage_name: str = "development"
     s3_bucket_name: str = "terraform-state-emperia-pdp"
-
+    database: str = "xxx"
 
 settings = Settings()
