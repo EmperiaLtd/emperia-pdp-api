1f4ffbf8f6b3b1d03aef226fa323e179d96d9f68
Shahzaib Asim <shahzaib@192.168.10.5>
2022-12-19 21:28:23 +0500

    adding pre-commit hook, refactoring all files

--- app/router/product.py
+++ app/router/product.py
@@ -1,13 +1,12 @@
-from fastapi import APIRouter, status, HTTPException
+import json
+
+from fastapi import APIRouter, HTTPException, status
+
 # import app.db.database as db
 import app.db.database as db
-from app.db.product import (
-    load_from_db,
-    load_from_db_2,
-    load_from_db_3,
-    market_name,
-)
-import json
+from app.db.product import (load_from_db, load_from_db_2, load_from_db_3,
+                            market_name)
+
 # database = db.redis
 router = APIRouter()
 # def ResponseModel(data, status_code, message):
@@ -18,35 +17,44 @@ router = APIRouter()
 #     }
 
 
-@router.get("/{org_id}/{market}/{pid}",
-            response_description="Product data retrieved")
+@router.get("/{org_id}/{market}/{pid}", response_description="Product data retrieved")
 async def get_product_data(pid, market, org_id):
     string = market_name(org_id, market, pid)
     product = load_from_db(pid, market, org_id, string)
     if product:
         file_1 = json.dumps(product)
         db.redis.set(string, file_1)
-        return {"data": product, "status": 200, "message":
-                "Products data retrieved successfully"}
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                        detail="Product doesn't exist.")
+        return {
+            "data": product,
+            "status": 200,
+            "message": "Products data retrieved successfully",
+        }
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="Product doesn't exist."
+    )
 
 
 @router.get("/{org_id}/{market}", response_description="Products retrieved")
 async def get_products(market, org_id):
     products = load_from_db_2(market, org_id)
     if products:
-        return {"data": products, "status": 200, "message":
-                "Products data retrieved successfully"}
+        return {
+            "data": products,
+            "status": 200,
+            "message": "Products data retrieved successfully",
+        }
     return {"data": products, "status": 200, "message": "Empty list returned"}
 
 
-@router.get("/{org_id}",
-            response_description="Market data retrieved")
+@router.get("/{org_id}", response_description="Market data retrieved")
 async def get_market(org_id):
     product = load_from_db_3(org_id)
     if product:
-        return {"data": product, "status": 200, "message":
-                "Market data retrieved successfully"}
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                        detail="Market doesn't exist.")
+        return {
+            "data": product,
+            "status": 200,
+            "message": "Market data retrieved successfully",
+        }
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="Market doesn't exist."
+    )
