96668da0ad853c9244e5890c7255ddc8f5a56374
Shahzaib Asim <shahzaib@Shahzaibs-MacBook-Pro.local>
2023-01-23 19:49:58 +0500

    adding dependency in modules

--- terraform/app-deploy/main.tf
+++ terraform/app-deploy/main.tf
@@ -6,7 +6,10 @@ module "iam_module" {
   stage = local.stage
 }
 
-module "api_gateway_module_us-east-1" {
+module "api_gateway_module_us_east_1" {
+  depends_on = [
+    module.iam_module
+  ]
   source = "./modules/api_gateway_module"
   providers = {
     aws = aws.us-east-1
@@ -16,7 +19,10 @@ module "api_gateway_module_us-east-1" {
    authorization_issuer   =  local.authorization_issuer
 }
 
-module "lambda_module_us-east-1" {
+module "lambda_module_us_east_1" {
+  depends_on = [
+    module.api_gateway_module_us_east_1
+  ]
   source = "./modules/lambda_module"
   providers = {
     aws = aws.us-east-1
@@ -25,18 +31,24 @@ module "lambda_module_us-east-1" {
   iam_role_lambda_arn = module.iam_module.iam_role_lambda_arn
 }
 
-module "api_gateway_integration_module_us-east-1" {
+module "api_gateway_integration_module_us_east_1" {
+  depends_on = [
+    module.lambda_module_us_east_1
+  ]
   source = "./modules/api_gateway_integration_module"
   providers = {
     aws = aws.us-east-1
   }
-  aws_lambda_function_arn = module.lambda_module_us-east-1.aws_lambda_function_arn
-  aws_lambda_function_invoke_arn = module.lambda_module_us-east-1.aws_lambda_function_invoke_arn
-  api_gateway_execution_arn = module.api_gateway_module_us-east-1.api_gateway_execution_arn
-  api_gateway_id = module.api_gateway_module_us-east-1.api_gateway_id
+  aws_lambda_function_arn = module.lambda_module_us_east_1.aws_lambda_function_arn
+  aws_lambda_function_invoke_arn = module.lambda_module_us_east_1.aws_lambda_function_invoke_arn
+  api_gateway_execution_arn = module.api_gateway_module_us_east_1.api_gateway_execution_arn
+  api_gateway_id = module.api_gateway_module_us_east_1.api_gateway_id
 }
 
-module "api_gateway_module_eu-west-2" {
+module "api_gateway_module_eu_west_2" {
+  depends_on = [
+    module.api_gateway_integration_module_us_east_1
+  ]
   source = "./modules/api_gateway_module"
   providers = {
     aws = aws.eu-west-2
@@ -45,7 +57,10 @@ module "api_gateway_module_eu-west-2" {
    authorization_audience =  local.authorization_audience
    authorization_issuer   =  local.authorization_issuer
 }
-module "lambda_module_eu-west-2" {
+module "lambda_module_eu_west_2" {
+  depends_on = [
+    module.api_gateway_module_eu_west_2
+  ]
   source = "./modules/lambda_module"
   providers = {
     aws = aws.eu-west-2
@@ -54,13 +69,16 @@ module "lambda_module_eu-west-2" {
   iam_role_lambda_arn = module.iam_module.iam_role_lambda_arn
 }
 
-module "api_gateway_integration_module_eu-west-2" {
+module "api_gateway_integration_module_eu_west_2" {
+  depends_on = [
+    module.lambda_module_eu_west_2
+  ]
   source = "./modules/api_gateway_integration_module"
   providers = {
     aws = aws.eu-west-2
   }
-  aws_lambda_function_arn = module.lambda_module_eu-west-2.aws_lambda_function_arn
-  aws_lambda_function_invoke_arn = module.lambda_module_eu-west-2.aws_lambda_function_invoke_arn
-  api_gateway_execution_arn = module.api_gateway_module_eu-west-2.api_gateway_execution_arn
-  api_gateway_id = module.api_gateway_module_eu-west-2.api_gateway_id
+  aws_lambda_function_arn = module.lambda_module_eu_west_2.aws_lambda_function_arn
+  aws_lambda_function_invoke_arn = module.lambda_module_eu_west_2.aws_lambda_function_invoke_arn
+  api_gateway_execution_arn = module.api_gateway_module_eu_west_2.api_gateway_execution_arn
+  api_gateway_id = module.api_gateway_module_eu_west_2.api_gateway_id
 }
