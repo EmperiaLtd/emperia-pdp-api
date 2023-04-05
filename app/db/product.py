9bc1c8000b37e60a0e8de655603266d635e72c7b
Shahzaib Asim <shahzaib@192.168.10.3>
2023-04-05 15:36:47 +0500

    changing p_name to p_key. using scan_iter instead of keys for increased performance. added support for JSON in redis.

--- app/db/product.py
+++ app/db/product.py
@@ -38,35 +38,35 @@ def getParameterFromAWS(key: str) -> str:
         )
 
 
-def create_solid(org_id, market, pid):
+def create_solid(org_id, market, p_key):
     if market == "US" or market == "us":
-        string = f"{org_id}_US_{pid}"
+        string = f"{org_id}_US_{p_key}"
     elif market == "CA" or market == "ca":
-        string = f"{org_id}_CA_{pid}"
+        string = f"{org_id}_CA_{p_key}"
     elif market == "INT" or market == "int":
-        string = f"{org_id}_INT_{pid}"
+        string = f"{org_id}_INT_{p_key}"
     else:
-        string = f"{org_id}_{market}_{pid}"
+        string = f"{org_id}_{market}_{p_key}"
     return string
 
 
-def connect_and_load_from_db(org_id, market, p_name, env):
+def connect_and_load_from_db(org_id, market, p_key, env):
     connect_to_db(org_id, env)
-    return load_from_db(org_id, market, p_name)
+    return load_from_db(org_id, market, p_key)
 
 
-def load_from_db(org_id, market, p_name):
-    solid = create_solid(org_id, market, p_name)
+def load_from_db(org_id, market, p_key):
+    solid = create_solid(org_id, market, p_key)
     print("getting solid", solid)
-    db_Obj = db.redis.get(solid)  # f"{org_id}_{market}_{pid}"
+    db_Obj = db.redis.json().get(solid)  # f"{org_id}_{market}_{pid}"
     if db_Obj:
         json_data = json.loads(db_Obj.encode("utf-8"))
         return json_data
     else:
-        return check_for_fall_back(org_id, market, p_name)
+        return check_for_fall_back(org_id, market, p_key)
 
 
-def check_for_fall_back(org_id: str, market: str, p_name: str):
+def check_for_fall_back(org_id: str, market: str, p_key: str):
     # fallback for pinko
     if org_id == "Pinko":
         data = read_csv_file("fallback_locales.csv")
@@ -75,14 +75,19 @@ def check_for_fall_back(org_id: str, market: str, p_name: str):
                 row.locale.lower() == market.lower()
                 and row.locale.lower() != row.defaultfeedlocale.lower()
             ):
-                return load_from_db(org_id, row.defaultfeedlocale.lower(), p_name)
+                return load_from_db(org_id, row.defaultfeedlocale.lower(), p_key)
 
 
 def load_pattern_from_db(solid, org_id, env):
     connect_to_db(org_id, env)
     print("getting solid", solid)
-    keys = db.redis.keys(solid)
-    products_response = db.redis.mget(keys)
+    # using scan here instead of keys to prevent DB blocking, results are same.
+    products_response = []
+    for key in db.redis.scan_iter(solid, 100):
+        print(key)
+        products_response.append(db.redis.json().get(key))
+
+    # products_response = db.redis.mget(keys)
     products = []
     for product in products_response:
         products.append(json.loads(product.encode("utf-8")))
