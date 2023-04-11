5564fa38232fe76cc7275401327bf5941f22d821
Shahzaib Asim <shahzaib@192.168.10.7>
2023-04-11 21:52:35 +0500

    Fixing encoding exception

--- app/db/product.py
+++ app/db/product.py
@@ -60,8 +60,7 @@ def load_from_db(org_id, market, p_key):
     print("getting solid", solid)
     db_Obj = db.redis.json().get(solid)  # f"{org_id}_{market}_{pid}"
     if db_Obj:
-        json_data = json.loads(db_Obj.encode("utf-8"))
-        return json_data
+        return db_Obj
     else:
         return check_for_fall_back(org_id, market, p_key)
 
