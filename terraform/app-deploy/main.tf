bb271d418b6aa9007b7077b2470c43b2102383d7
Shahzaibemperia <shahzaib@emperiavr.com>
2024-05-09 21:54:19 +0500

    Recreating eu-west-2 resources without changing gateway

--- terraform/app-deploy/main.tf
+++ terraform/app-deploy/main.tf
@@ -57,35 +57,35 @@ module "iam_module" {
 #    authorization_audience =  local.authorization_audience
 #    authorization_issuer   =  local.authorization_issuer
 # }
-# module "lambda_module_eu_west_2" {
-#   depends_on = [
-#     module.api_gateway_module_eu_west_2
-#   ]
-#   source = "./modules/lambda_module"
-#   providers = {
-#     aws = aws.eu-west-2
-#   }
-#   stage = local.stage
-#   iam_role_lambda_arn = module.iam_module.iam_role_lambda_arn
-# }
+module "lambda_module_eu_west_2" {
+  depends_on = [
+    module.iam_module
+  ]
+  source = "./modules/lambda_module"
+  providers = {
+    aws = aws.eu-west-2
+  }
+  stage = local.stage
+  iam_role_lambda_arn = module.iam_module.iam_role_lambda_arn
+}
 
-# module "api_gateway_integration_module_eu_west_2" {
-#   depends_on = [
-#     module.lambda_module_eu_west_2
-#   ]
-#   source = "./modules/api_gateway_integration_module"
-#   providers = {
-#     aws = aws.eu-west-2
-#   }
-#   aws_lambda_function_arn = module.lambda_module_eu_west_2.aws_lambda_function_alias_arn
-#   aws_lambda_function_invoke_arn = module.lambda_module_eu_west_2.aws_lambda_function_alias_invoke_arn
-#   api_gateway_execution_arn = module.api_gateway_module_eu_west_2.api_gateway_execution_arn
-#   api_gateway_id = module.api_gateway_module_eu_west_2.api_gateway_id
-# }
+module "api_gateway_integration_module_eu_west_2" {
+  depends_on = [
+    module.lambda_module_eu_west_2
+  ]
+  source = "./modules/api_gateway_integration_module"
+  providers = {
+    aws = aws.eu-west-2
+  }
+  aws_lambda_function_arn = module.lambda_module_eu_west_2.aws_lambda_function_alias_arn
+  aws_lambda_function_invoke_arn = module.lambda_module_eu_west_2.aws_lambda_function_alias_invoke_arn
+  api_gateway_execution_arn = "arn:aws:execute-api:eu-west-2:601772211076:dyeaqvtbs3/*/*"
+  api_gateway_id = "dyeaqvtbs3"
+}
 
 module "api_gateway_module_ap_south_1" {
   depends_on = [
-    module.iam_module
+    module.api_gateway_integration_module_eu_west_2
   ]
   source = "./modules/api_gateway_module"
   providers = {
